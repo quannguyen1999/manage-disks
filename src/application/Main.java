009a3b59a84385fb0be646f2de8db4ea51ab37f2
quannguyen1999 <nguyendanganhquan99@gmail.com>
2020-09-04 23:40:27 +0700

    init data login.

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,25 +1,54 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.Color;
 
 
 public class Main extends Application {
+	private double xOffset=0;
+	
+	private double YOffset=0;
+	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			Parent root=(Parent) FXMLLoader.load(getClass().getResource("fxml/Login.fxml"));
+			
+			root.setOnMousePressed((MouseEvent event)->{
+				xOffset=event.getSceneX();
+				YOffset=event.getSceneY();
+			});
+			root.setOnMouseDragged((MouseEvent event)->{
+				primaryStage.setX(event.getScreenX()-xOffset);
+				primaryStage.setY(event.getScreenY()-YOffset);
+			});
+			
+			root.setEffect(new DropShadow(10, Color.PURPLE));
+			
+			Scene scene = new Scene(root);
+			scene.setFill(Color.TRANSPARENT);
 			primaryStage.setScene(scene);
+//			new FadeInUp(root).play();
+			primaryStage.setResizable(true);
+//			new animatefx.animation.FadeIn(root).play();
+			primaryStage.initStyle(StageStyle.TRANSPARENT);
+			
+			
 			primaryStage.show();
+		
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
---
+++ src/application/controller/Login.java
@@ -0,0 +1,22 @@
+package application.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+
+public class Login implements Initializable{
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public void btnExit(ActionEvent e) {
+		System.exit(0);
+	}
+
+}
